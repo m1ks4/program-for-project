55c859b73b3f07573d0e2881f9ccef6976900df7
v <ivan.khromenko@gmail.com>
2024-12-16 22:01:35 +0300

    maybe the last version of the site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,18 @@
 import {useState} from 'react';
-import {Button} from 'primereact/button';
+import {useEffect} from 'react';
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
-import {BreadCrumb} from 'primereact/breadcrumb';
-import React, { useEffect } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { ProductService } from './service/ProductService';
-
+import { Timeline } from 'primereact/timeline';
 
 
 function App() {
-    const [count,setCount] = useState(0);
-    
-    const [data,setData] = useState({});
-
-    const items = [{ label: 'Electronics' }, { label: 'Computer' }, { label: 'Accessories' }, { label: 'Keyboard' }, { label: 'Wireless' }];
-
-    const home = { icon: 'pi pi-home', url: 'https://primereact.org' }
-
-    const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        ProductService.getProductsMini().then(data => setProducts(data));  
+        getData();
     }, []);
-                            
+
     function getData(){
         fetch('http://localhost:3080/')
             .then( res => res.json())
@@ -35,31 +21,79 @@ function App() {
                 setData(json)
             })
     }
+    function showContent(item, ){
+        return <div>{item.time} &nbsp; {item.class}</div>
+    
+    }
 
-    return (
-        <div>
-            <div className="text-center">
-                <Button label="Click" icon="pi pi-plus" onClick={e => setCount(count + 1)}></Button>
-                <Button label="Get data" icon="pi pi-plus" onClick={getData}></Button>
-                <div className="text-2xl text-900 mt-3">{count}</div> {/* className == имена css классов */}
-                <div className="text-2xl text-900 mt-3">{JSON.stringify(data)}</div>
-                <BreadCrumb model={items} home={home}/>
+
+    const [data,setData] = useState();
+
+    return(
+        <div className="card flex flex-column gap-3">
+            <div class="overflow-hidden">
+                <div class="flex flex-wrap align-items-center justify-content-center">
+                    <h3>431 кабинет &nbsp; &nbsp;</h3>
+                    {data &&
+                        <Timeline value={data.slice(0, 8)} layout="horizontal" align="top" content={showContent} opposite={(item) => item.lesson}/>
+                    }
+                </div>
             </div>
-            <div className="card">
-                <DataTable value={products} tableStyle={{ minWidth: '50rem' }}>
-                    <Column field="floor" header="Floor"/>
-                    <Column field="classroom" header="Classroom"/>
-                    <Column field="time" header="Time"/>
-                    <Column field="lesson" header="Lesson"/>
-                </DataTable>
+            <div class="overflow-hidden">
+                <div class="flex flex-wrap align-items-center justify-content-center">
+                <h3>452 кабинет &nbsp; &nbsp;</h3>
+                    {data &&
+                        <Timeline value={data.slice(8, 16)} layout="horizontal" align="top" content={showContent} opposite={(item) => item.lesson}/>
+                    }
+                </div>
             </div>
-            <div className="card">
-                <DataTable value={data} tableStyle={{ minWidth: '50rem' }}>
-                    <Column field="floor" header="Floor"/>
-                    <Column field="classroom" header="Classroom"/>
-                    <Column field="time.one" header="Time"/>
-                    <Column field="lesson.first" header="Lesson"/>
-                </DataTable>
+            <div class="overflow-hidden">
+                <div class="flex flex-wrap align-items-center justify-content-center">
+                <h3>444 кабинет &nbsp; &nbsp;</h3>
+                    {data &&
+                        <Timeline value={data.slice(16, 24)} layout="horizontal" align="top" content={showContent} opposite={(item) => item.lesson}/>
+                    }
+                </div>
+            </div>
+            <div class="overflow-hidden">
+                <div class="flex flex-wrap align-items-center justify-content-center">
+                <h3>456 кабинет &nbsp; &nbsp;</h3>
+                    {data &&
+                        <Timeline value={data.slice(24, 32)} layout="horizontal" align="top" content={showContent} opposite={(item) => item.lesson}/>
+                    }
+                </div>
+            </div>
+            <div class="overflow-hidden">
+                <div class="flex flex-wrap align-items-center justify-content-center">
+                <h3>421 кабинет &nbsp; &nbsp;</h3>
+                    {data &&
+                        <Timeline value={data.slice(32, 40)} layout="horizontal" align="top" content={showContent} opposite={(item) => item.lesson}/>
+                    }
+                </div>
+            </div>
+            <div class="overflow-hidden">
+                <div class="flex flex-wrap align-items-center justify-content-center">
+                <h3>487 кабинет &nbsp; &nbsp;</h3>
+                    {data &&
+                        <Timeline value={data.slice(40, 48)} layout="horizontal" align="top" content={showContent} opposite={(item) => item.lesson}/>
+                    }
+                </div>
+            </div>
+            <div class="overflow-hidden">
+                <div class="flex flex-wrap align-items-center justify-content-center">
+                <h3>499 кабинет &nbsp; &nbsp;</h3>
+                    {data &&
+                        <Timeline value={data.slice(48, 56)} layout="horizontal" align="top" content={showContent} opposite={(item) => item.lesson}/>
+                    }
+                </div>
+            </div>
+            <div class="overflow-hidden">
+                <div class="flex flex-wrap align-items-center justify-content-center">
+                    <h3>455 кабинет &nbsp; &nbsp;</h3>
+                    {data &&
+                        <Timeline value={data.slice(56, 64)} layout="horizontal" align="top" content={showContent} opposite={(item) => item.lesson}/>
+                    }
+                </div>
             </div>
         </div>
     );
@@ -67,5 +101,45 @@ function App() {
 
 export default App;
 
+/*
+const user = {
+    name: 'John Wick',
+    imageUrl: 'https://png.pngtree.com/thumb_back/fw800/background/20230527/pngtree-phoenix-bird-in-flames-wallpapers-wallpapershd-image_2697352.jpg',
+    imageSize: 900,
+};
+
+function MyButton() {
+    return (
+    <button>
+        Click me
+    </button>
+    );
+}
+
+export default function MyApp() {
+    const [count,setCount] = useState(0);
+    return (
+        <div>
+            <>
+            <h1>{user.name}</h1>
+            <img
+            className="avatar"
+            src={user.imageUrl}
+            alt={'Photo of ' + user.name}
+            style={{
+                width: user.imageSize,
+                height: user.imageSize
+            }}
+            />
+            </>
+            <>
+            <MyButton onClick={e => setCount(count+1)}>
+                <div className="text-2xl text-900 mt-3">Clicked {count} times</div>
+            </MyButton>
+            </>
+
+        </div>
         
-        
+    );
+}
+*/
